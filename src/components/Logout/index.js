7e92d22e1a506cf909c0dc2141211bd694a87d14
rajashekarpodicheti <rajashekarpodicheti1@gmail.com>
2023-04-04 16:31:47 +0000

    BoxigoApp

---
+++ src/components/Logout/index.js
@@ -0,0 +1,11 @@
+const Logout = () => {
+  document.title = 'Logout'
+  return (
+    <div>
+      <h1>Logout</h1>
+      <p>Your logged out Successfully </p>
+    </div>
+  )
+}
+
+export default Logout
